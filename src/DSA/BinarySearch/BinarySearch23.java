3c9270ca01b7f3286dcd5d81cd1053c39ac4160d
Rajat <rajatkumarsaho638@gmail.com>
2024-01-26 17:48:15 +0530

    Binary Search

--- src/DSA/BinarySearch/BinarySearch23.java
+++ src/DSA/BinarySearch/BinarySearch23.java
@@ -5,33 +5,19 @@ public class BinarySearch23 {
     public static void main(String[] args) {
         int []arr = {2,3,5,6,7,8,9,13,45,67,89};
         int target = 5;
-        int ans = findingRange(arr,target);
+        int ans = BinarySearch(arr,target);
         System.out.println("The target value index is " + ans);
     }
 
-    static   int findingRange(int[]arr,int target){
-
-        int start=0;
-        int end =1;
-        while (target>arr[end]){
-            int temp = end + 1; // this is my new start
-            // double the box value
-            // end = previous end + sizeofbox*2
-            end = end + (end - start + 1) * 2; // start=end+1;end=start*2+1;
-            start = temp;
-        }
-        return BinarySearch(arr,target,start,end);
-    }
 
 
 
 
 
-    public  static  int BinarySearch(int[]arr,int target,int start,int end){
+    public  static  int BinarySearch(int[]arr,int target){
 
-//        int si=start;
-//        int  ei = end;
-
+        int start =0;
+        int end= arr.length;
 
         while(start <= end) {
             // find the middle element
--- src/DSA/BinarySearch/InfiniteArrayBinarySearch27.java
+++ src/DSA/BinarySearch/InfiniteArrayBinarySearch27.java
@@ -2,6 +2,40 @@ package DSA.BinarySearch;
 
 public class InfiniteArrayBinarySearch27 {
     public static void main(String[] args) {
+        int  []arr = {1,2,3,4,5,6,7,8,10,1234,45};
+        int target = 3;
+        int ans = Findingrange(arr,target);
+        System.out.println(ans);
+
 
     }
+
+    static  int Findingrange(int[]arr,int target){
+       int start=0;
+       int end=1;
+
+       while(target>arr[end]){
+           start=end+1;
+           end=start*2+1;
+       }
+       return  BinarySearch(arr,target,start,end);
+    }
+    static  int BinarySearch(int[]arr,int target,int start,int end) {
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+
 }

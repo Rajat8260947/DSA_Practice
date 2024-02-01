9219748802e50abab6dc064abb184449c29e8494
Rajat <rajatkumarsaho638@gmail.com>
2024-02-01 22:06:03 +0530

    sorting

---
+++ src/DSA/Sorting/Normalsorting.java
@@ -0,0 +1,15 @@
+package DSA.Sorting;
+
+import java.util.Arrays;
+
+public class Normalsorting {
+    public static void main(String[] args) {
+        int [] arr = {1,3,24,2,4,21,3,4,123,45};
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+
+}

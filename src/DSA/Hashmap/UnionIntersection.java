e778dd91fa6318e8b3eb22878fa58dcfa4c74f80
Rajat <rajatkumarsaho638@gmail.com>
2024-02-12 18:51:26 +0530

    Hasing

---
+++ src/DSA/Hashmap/UnionIntersection.java
@@ -0,0 +1,39 @@
+package DSA.Hashmap;
+
+import java.util.HashSet;
+
+public class UnionIntersection {
+    public static void main(String[] args) {
+        int[]arr1 = {7,3,9};
+        int[]arr2= {6,3,9,6,2,4};
+        int res1 = Union(arr1,arr2);
+        System.out.println(res1);
+        int res2 = Intesection(arr1,arr2);
+        System.out.println(res2);
+    }
+    static  int Union (int[]arr1,int[]arr2){
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+           hs.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+           hs.add(arr2[i]);
+        }
+        return hs.size();
+    }
+    static  int Intesection (int[]arr1,int[]arr2){
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++){
+            hs.add(arr1[i]);
+        }
+        int count=0;
+        for (int i = 0; i <arr2.length ; i++) {
+            if(hs.contains(arr2[i])){
+                count++;
+                hs.remove(arr2[i]);
+            }
+
+        }
+     return  count;
+    }
+}

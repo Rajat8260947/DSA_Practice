6e153972e573b941d94a732e5ab9d5861d7e518a
Rajat <rajatkumarsaho638@gmail.com>
2024-02-26 23:44:07 +0530

    linklist

---
+++ src/DSA/Linklist/Main.java
@@ -0,0 +1,39 @@
+package DSA.Linklist;
+
+public class Main {
+    public static void main(String[] args) {
+//        LL list = new LL();
+//        list.insertFirst(3);
+//        list.insertFirst(4);
+//        list.insertFirst(5);
+//        list.insertFirst(6);
+//        list.insertFirst(7);
+//        list.insertLast(99);
+//        list.insert(100,3);
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.deletePosition(3));
+//        list.display();
+//        DLL list = new DLL();
+//        list.insertFirst(3);
+//        list.insertFirst(4);
+//        list.insertFirst(5);
+//        list.insertFirst(6);
+//        list.insertFirst(7);
+//        list.insertLast(99);
+//        list.insertAfter(99,100);
+//        list.display();
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(24);
+        list.insert(34);
+        list.insert(56);
+        list.insert(87);
+        list.display();
+
+    }
+
+}

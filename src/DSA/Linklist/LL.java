8c0d4576d186eb6bb9afa97b391e6428b0390384
Rajat <rajatkumarsaho638@gmail.com>
2024-02-29 23:25:24 +0530

    Linklist

--- src/DSA/Linklist/LL.java
+++ src/DSA/Linklist/LL.java
@@ -132,4 +132,76 @@ public class LL {
 
     }
 
+    public void duplicateRemove(){
+        Node node = head;
+        while (node.next!=null){
+            if(node.value==node.next.value){
+                node.next= node.next.next;
+                size--;
+            }
+            else {
+                node= node.next;
+            }
+        }
+
+    }
+
+    // merge two linklist
+
+    public static  LL merge(LL first ,LL second){
+
+        LL ans = new LL();
+        Node f = first.head;
+        Node  s = second.head;
+
+        while(f!=null && s!=null){
+            if(f.value<s.value){
+                ans.insertLast(f.value);
+                f=f.next;
+            }
+            else {
+                ans.insertLast(s.value);
+                s=s.next;
+
+            }
+        }
+        while(f!=null){
+            ans.insertLast(f.value);
+            f=f.next;
+        } while(s!=null){
+            ans.insertLast(s.value);
+            s=s.next;
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+//        LL list = new  LL();
+//        list.insertLast(1);
+//        list.insertLast(1);
+//        list.insertLast(1);
+//        list.insertLast(2);
+//        list.insertLast(3);
+//        list.insertLast(3);
+//        list.display();
+//        list.duplicateRemove();
+//        list.display();
+
+        LL first = new LL();
+        LL  second = new LL();
+
+        first.insertLast(1);
+        first.insertLast(3);
+        first.insertLast(4);
+        second.insertLast(1);
+        second.insertLast(2);
+        second.insertLast(9);
+        second.insertLast(14);
+
+        LL ans = LL.merge(first,second);
+        ans.display();
+
+    }
+
 }

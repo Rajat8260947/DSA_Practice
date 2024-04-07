2dafae1d17e445190dfe6a42c2ef5e22eefa4972
Rajat <rajatkumarsaho638@gmail.com>
2024-04-07 08:31:50 +0530

    stack

---
+++ src/DSA/Stack/BalanceBracket.java
@@ -0,0 +1,36 @@
+package DSA.Stack;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BalanceBracket {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string ");
+        String str = sc.nextLine();
+
+        System.out.println(isBalanced(str));
+    }
+
+    public static  boolean isBalanced(String str ){
+
+        Stack<Character> st = new Stack<>();
+         int n = str.length();
+        for (int i = 0; i <n ; i++) {
+            char ch = str.charAt(i);
+            if(ch=='('){
+                st.push(ch);
+            }
+            else {
+                if(st.size()==0) return  false;
+                if(st.peek()=='(') st.pop();
+            }
+
+        }
+
+        if(st.size()>0) return  false;
+        return  true;
+
+    }
+}

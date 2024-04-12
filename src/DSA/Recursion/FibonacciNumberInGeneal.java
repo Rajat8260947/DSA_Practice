bc12f9a6db4753a35bcfa6a5d4bb78e1a6f7c5a5
Rajat <rajatkumarsaho638@gmail.com>
2024-04-12 18:11:28 +0530

    changes

--- src/DSA/Recursion/FibonacciNumberInGeneal.java
+++ src/DSA/Recursion/FibonacciNumberInGeneal.java
@@ -10,7 +10,7 @@ public class FibonacciNumberInGeneal {
         int n1=0;
         int n2=1;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n; i++) {
             System.out.print(n1 + " ");
             int n3= n1+n2;
             n1=n2;
@@ -18,6 +18,6 @@ public class FibonacciNumberInGeneal {
 
         }
 
-        System.out.println(n1);
+//        System.out.println(n1);
     }
 }

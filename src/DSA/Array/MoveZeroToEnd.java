0bed83990b718bb7000830073e75138d1810babc
Rajat <rajatkumarsaho638@gmail.com>
2024-04-17 07:18:54 +0530

    practice

--- src/DSA/Array/MoveZeroToEnd.java
+++ src/DSA/Array/MoveZeroToEnd.java
@@ -12,8 +12,25 @@ public class MoveZeroToEnd {
     }
 
      static    int[] moveZeros(int n,int[] arr){
+//        int j=-1;
+//         for (int i = 0; i < n; i++) {
+//             if(arr[i]==0){
+//                 j=i;
+//                 break;
+//             }
+//
+//         }
+//
+//         if(j==-1){
+//             return arr;
+//         }
+//         or for j =0
          int j=0;
-         for(int i=0;i<n;i++)
+
+//         for(int i=j+1;i<n;i++) for j=-1;
+         for (int i = 0; i < n; i++)
+
+
          {
              if(arr[i]!=0)
              {

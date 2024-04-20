07971eccccf7ff35e00ac5d7469ee8005776d120
Rajat <rajatkumarsaho638@gmail.com>
2024-04-20 18:32:20 +0530

    practice

---
+++ src/DSA/Array/Union.java
@@ -0,0 +1,27 @@
+// gfg pratice
+
+/*
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        // add your code here
+        HashSet<Integer>st = new HashSet<>();
+
+        for(int i=0;i<n;i++){
+            st.add(arr1[i]);
+        }
+        for(int i=0;i<m;i++){
+            st.add(arr2[i]);
+        }
+
+        ArrayList<Integer> list = new  ArrayList<>(st);
+        Collections.sort(list);
+        return list ;
+    }
+}
+
+
+*/
+

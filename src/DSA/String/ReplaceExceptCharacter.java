2958c3c3e8b909dcabb294c16dbd80fe7c1d95ad
Rajat <rajatkumarsaho638@gmail.com>
2024-07-30 23:09:41 +0530

    practice

---
+++ src/DSA/String/ReplaceExceptCharacter.java
@@ -0,0 +1,15 @@
+package DSA.String;
+
+//public class ReplaceExceptCharacter {
+//     static void main(String[] args) {
+//        Teacher t = new Student ();
+//         System.out.println(t.name);
+//
+//    }
+//}
+//class  Teacher {
+//    String name ="Sam";
+//}
+//class  Student {
+//    String name ="Kevin";
+//}

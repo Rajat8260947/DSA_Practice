4eeb1c213f59f9015e0d0e253cd7856e0ba7fa0c
Rajat <rajatkumarsaho638@gmail.com>
2024-08-01 23:04:20 +0530

    practice

---
+++ src/DSA/String/Vowel.java
@@ -0,0 +1,14 @@
+package DSA.String;
+
+public class Vowel {
+    public static void main(String[] args) {
+        char ch = 'b';
+        if(ch=='a'||ch=='e'|| ch=='i'|| ch=='o' || ch=='u'){
+            System.out.println("character is vowel");
+        }
+        else {
+            System.out.println("character is not a vowel");
+        }
+    }
+
+}

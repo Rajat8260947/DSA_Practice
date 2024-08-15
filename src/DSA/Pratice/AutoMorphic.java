b3a87aed4bed2f5ae2059da0e5f117340227f68a
Rajat <rajatkumarsaho638@gmail.com>
2024-08-15 09:12:44 +0530

    practice

---
+++ src/DSA/Pratice/AutoMorphic.java
@@ -0,0 +1,25 @@
+package DSA.Pratice;
+
+public class AutoMorphic {
+    public static void main(String[] args) {
+        int num =  25;
+          int res = (int) Math.pow(num,2);
+              while (num !=0) {
+
+
+                  if (num % 10 != res % 10) {
+                      System.out.println("The Number is not automorphic");
+                  }
+
+                  num =num/10;
+                  res =res/10;
+              }
+
+
+
+
+               System.out.println("The Number is automorphic");
+
+
+    }
+}
